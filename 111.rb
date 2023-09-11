68de6ce1535a39cfa140138ee205cd567a321cc6
askarf <ayalakarf@gmail.com>
2023-09-11 12:57:24 -0400

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+
+# QUESTION 1
+puts 10 - 6
+puts 10 * 3
+puts 24 / 2
+puts 900 + 83
+
+# QUESTION 2
+puts 239 x 9350
+# error111.rb:19: syntax error, unexpected tIDENTIFIER, expecting end-of-input puts 239 x 9350
+#  on line 19 in the file 111.rb, there is an invalid method with puts 239 x 9350
