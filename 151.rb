4161de9de4b05929d523d22ec282f8e86a92f541
askarf <ayalakarf@gmail.com>
2023-09-15 10:24:40 -0400

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,35 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+
+def display_ayala_karf
+    puts"                      _         _  __           __  "
+    puts"     /\              | |       | |/ /          / _| "
+    puts"    /  \  _   _  __ _| | __ _  | ' / __ _ _ __| |_  "
+    puts"   / /\ \| | | |/ _` | |/ _` | |  < / _` | '__|  _| "
+    puts"  / ____ \ |_| | (_| | | (_| | | . \ (_| | |  | |   "
+    puts" /_/    \_\__, |\__,_|_|\__,_| |_|\_\__,_|_|  |_|   "
+    puts"           __/ |                                    "
+    puts"          |___/                                     "
+    puts""
+end
+
+display_ayala_karf
+display_ayala_karf
+display_ayala_karf
+
+
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+  end
+
+print_lyrics
