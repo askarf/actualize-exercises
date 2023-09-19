b7cc07d4011f66283193360898a5e055755816f7
askarf <ayalakarf@gmail.com>
2023-09-19 08:47:59 -0400

    Add exercize solutions

---
+++ 221.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines
+
+person = {"first_name" => "ayala", "last_name" => "karfunkel", "email" => "ayalakarf@gmail.com"}
+pp person["first_name"]
+pp person["last_name"]
+pp person["email"]
+
+
+
+
+# Were you able to easily solve the problem from memory? no, needed to look at the solution
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice. i forgot what sign to use, i was usting = instead of =>, im going to practice the rest of these questions from arrays-hashes-2
+
+
