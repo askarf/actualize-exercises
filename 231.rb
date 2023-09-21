8d78a7e119bc318bd85c5ca42b91d88dc8c04ddb
askarf <ayalakarf@gmail.com>
2023-09-20 22:08:44 -0400

    Add exercise solutions

---
+++ 231.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory? no
+# If so, describe the next skill you will start to practice tomorrow. 
+# If not, describe the part you got stuck on that requires more practice. i got confused between pre existing methods that go with strings, and creating class's instead of creating methods
+
+
+# Write a method that takes in a number and returns the number times two. Then run the method and print the result
+
+def double_num(number)
+    return number *2
+end
+
+pp double_num(5)
+
