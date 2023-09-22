4c400d618259b2e4be6d5ff21e7f8e68ad69e554
askarf <ayalakarf@gmail.com>
2023-09-22 12:51:49 -0400

    Add exercise solutions

---
+++ 253.rb
@@ -0,0 +1,32 @@
+# QUESTION 1
+# Revise the Song class so that it reflects idiomatic Ruby conventions.
+class Song
+    attr_reader :title, :artist, :lyrics
+    attr_writer :title, :artist, :lyrics
+
+    def initialize(input_title, input_artist, input_lyrics)
+      @title = input_title
+      @artist = input_artist
+      @lyrics = input_lyrics
+    end
+  end
+  
+
+  
+  # QUESTION 2
+  # Fix the errors in the code below.
+class Person
+    attr_reader :name, :height
+    
+    def initialize(name, height)
+        @name = name
+        @height = height
+    end
+end
+  
+  person = Person.new("Rob", 80)
+  pp person
+  pp person.name
+  pp person.height
+
+  
