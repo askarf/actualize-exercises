b69a98351974b2c44f91da71abbe40b9e9a4f834
askarf <ayalakarf@gmail.com>
2023-09-22 09:01:37 -0400

    Add exercise solutions

---
+++ 251.rb
@@ -0,0 +1,23 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a Song class with attributes for name, artist, and duration.
+
+class Song
+    def initialize(name, artist, duration)
+        @name = name
+        @artist = artist
+        @duration = duration
+    end
+end
+
+song1 = Song.new("luv you", "ayala", 34.54)
+pp song1
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
