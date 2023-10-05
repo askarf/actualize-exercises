b4f8712cec1816dd964286d16c8bd98df9935a0d
askarf <ayalakarf@gmail.com>
2023-10-05 09:01:41 -0400

    Add exercise solutions

---
+++ 442.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns an array with every number doubled.
+# Then run the method and print the result.
+
+numbers = [334, 1, 65, 34, 5, 7, 876]
+numbers2 = []
+
+numbers.each do |number|
+    numbers2 << number * 2
+end
+
+pp numbers2
+
+
+
+
+
+
+
