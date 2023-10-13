b189e66e5439c0abaa3c863ea2bb549ac098ffc6
askarf <ayalakarf@gmail.com>
2023-10-13 16:05:55 -0400

    Add exercise solutions

--- 551.rb
+++ 551.rb
@@ -3,7 +3,7 @@
 # Write the code to solve the problem without looking at any notes or other resources.
 
 
-# Were you able to easily solve the problem from memory?
+# Were you able to easily solve the problem from memory? yes
 # If so, describe the next skill you will start to practice tomorrow.
 # If not, describe the part you got stuck on that requires more practice.
 
---
+++ 552.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Write a method that takes in three strings and uses string interpolation 
+# to return all three strings combined with spaces in between as a single string. 
+# Then run the method and print the result.
+
+
+name1 = "Ayala"
+name2 = "Mimi"
+name3 = "Sarala"
+pp "My three best friends names are #{name1}, #{name2}, and #{name3}"
